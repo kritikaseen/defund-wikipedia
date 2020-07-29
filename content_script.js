bd3d4cb177ac270ff168ab1f6ee58cd132805374
Kritika Sen <66664605+kritikaseen@users.noreply.github.com>
2020-07-29 11:01:51 +0530

    Added files via upload.

---
+++ content_script.js
@@ -0,0 +1,5 @@
+var element = document.querySelector('div#siteNotice');
+element.parentElement.removeChild(element);
+
+element = document.querySelector('div#frb-inline');
+element.parentElement.removeChild(element);
